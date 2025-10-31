428caf841082c9663f371a68a2e0a12c8b98ee95
Balamurugan S <balamurugans27102004@gmail.com>
2025-10-31 18:14:33 +0530

    Time: 5 ms (77.74%), Space: 77.4 MB (8.1%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l=0,r=height.length-1,max=0;
+        while(l<r){
+            int h=Math.min(height[l],height[r]);
+            max=Math.max(max,h*(r-l));
+            if(height[l]>=height[r]){
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        return max;
+    }
+}
